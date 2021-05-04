936b9f9c6b4057c4b04037ce9eacf0429221dae4
John Paulo Panganiban <olopangs@gmail.com>
2021-05-04 19:01:28 +0800

    firebase authentication and live db

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from 'firebase'
 const firebaseConfig = {
     apiKey: "AIzaSyDeIqPW3xJ9YsFmhULXxnDwNb-4Lh8vdlE",
     authDomain: "slackclone-3b51f.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:902893110555:web:88197c954acf30cc7effbe",
     measurementId: "G-T6S9TYCKN6"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig)
+  // access database
+  const db = firebaseApp.firestore()
+  const auth = firebase.auth()
+  const provider = new firebase.auth.GoogleAuthProvider()
+
+  export { auth, provider, db }
