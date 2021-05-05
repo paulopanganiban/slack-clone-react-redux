85b840af0235ff20c4ad785f86b050711d56fabf
John Paulo Panganiban <olopangs@gmail.com>
2021-05-05 21:33:44 +0800

    chat input

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,31 +1,79 @@
 import React from 'react'
 import styled from 'styled-components'
+import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import { useSelector } from 'react-redux';
+import { selectRoomId } from '../features/appSlice';
+import ChatInput from './ChatInput';
 const Chat = () => {
+
+    const roomId = useSelector(selectRoomId)
     return (
         <ChatContainer>
-            <h1>chat</h1>
-            <Header>
-                <HeaderLeft>
+            <>
+                <Header>
+                    <HeaderLeft>
+                        <h4><strong>#Room Name   </strong> <StarBorderOutlinedIcon /></h4>
 
-                </HeaderLeft>
-                <HeaderRight>
+                    </HeaderLeft>
+                    <HeaderRight>
+                        <p>
+                            <InfoOutlinedIcon /> Details
+                    </p>
+                    </HeaderRight>
+                </Header>
 
-                </HeaderRight>
-            </Header>
+
+                <ChatMessages>
+                    {/* List out messages */}
+                </ChatMessages>
+
+                <ChatInput channelId={roomId}>
+                   
+                </ChatInput>
+            </>
+
         </ChatContainer>
     )
 }
 
 export default Chat
-const HeaderRight = styled.div`
 
+const ChatMessages = styled.div`
+
+`
+
+const HeaderRight = styled.div`
+ > p {
+     display: flex;
+     align-items: center;
+     font-size: 14px;
+ }
+ > p > .MuiSvgIcon-root {
+     margin-right: 5px !important;
+     font-size: 16px;
+ }
 `
 
 const HeaderLeft = styled.div`
-
+display: flex;
+align-items: center;
+ > h4 {
+     display: flex;
+     text-transform: lowercase;
+     margin-right: 10px;
+ }
+ > h4 > .MuiSvgIcon-root {
+     padding-top: 2px;
+     margin-left: 10px;
+     font-size: 18px;
+ }
 `
 const Header = styled.div`
-
+display: flex;
+justify-content: space-between;
+padding: 20px;
+border-bottom: 1px solid lightgray;
 `
 const ChatContainer = styled.div`
 flex: 0.7;
---
+++ src/components/ChatInput.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Input } from '@material-ui/core';
+import styled from 'styled-components'
+const ChatInput = () => {
+    return (
+        <div>
+            <h1>2:07:10</h1>
+        </div>
+    )
+}
+
+export default ChatInput
+
+const ChatInputContainer = styled.div`
+
+`
